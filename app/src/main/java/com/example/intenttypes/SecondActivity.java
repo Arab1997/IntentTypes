9ff741bcae7dbe6d1a9264837e2aece9d01f29da
Abdulloh <maxmudovabdullo97@gmail.com>
2020-07-21 13:00:13 +0500

    Lesson3 4

---
+++ app/src/main/java/com/example/intenttypes/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.intenttypes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView  textView=(TextView) findViewById(R.id.txt_bundle);
+        Bundle bundle=getIntent().getExtras();
+        String data=bundle.get("data").toString();
+        textView.setText(data);
+    }
+}
